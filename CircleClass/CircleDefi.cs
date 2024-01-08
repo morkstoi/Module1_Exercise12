73580439fe8cdd851ebd93eb1c2186b345db9180
theac <theac@DESKTOP-LEARA2L>
2024-01-08 18:37:10 +0300

    Add project files.

---
+++ CircleClass/CircleDefi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleClass
+{
+    static class CircleDefi
+    {
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetArea(double r)
+        {
+            return  Math.PI * r*r;
+        }
+        static public bool IsWithinBounadries(double x1,double y1, double x0, double y0, double r)
+        {
+            double distance = Math.Sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+            return distance <= r;
+        }
+
+    }
+}
---
+++ CircleClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности:");
+            double r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координату точки x1:");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координату точки y1:");
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            double x0 = 0;
+            double y0 = 0;
+            double circleArea = CircleDefi.GetArea(r);
+            double circleLength = CircleDefi.GetLength(r);
+            bool withinBoundaries = CircleDefi.IsWithinBounadries(x1, y1, x0, y0, r);
+            Console.WriteLine($"Площадь круга равна:{circleArea}.");
+            Console.WriteLine($"Длина окружности равна:{circleLength}.");
+            Console.WriteLine($"Точка с координатами ({x1},{y1}) находится внутри окружности:{withinBoundaries}.");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
